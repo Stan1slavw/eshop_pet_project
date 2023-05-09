e202aa7f2458d82339b85920dc84721d0c3b50f9
Stan1slav <stas1234713@gmail.com>
2023-05-09 22:54:15 +0300

    +Stas, user, product, image entity. add application properties

---
+++ src/main/java/com/example/mpps/entity/Product.java
@@ -0,0 +1,51 @@
+package com.example.mpps.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+    @Column(name = "price")
+    private String price;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "category")
+    private String category;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+    mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
+}
